6bf9e27602909d2f65f93f6ae1663dad58afc204
Pawel Pluta <pluta.p@gmail.com>
2020-02-22 21:55:33 +0100

    CORS

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var cors = require('cors');
 var app = express();
-
+app.use(cors());
 app.use(express.static(__dirname));
 
 // app.use(function (req, res, next) {
