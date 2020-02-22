bcd11db60489ae711d411f4cd01d4a7a74c0a1ce
Pawel Pluta <pluta.p@gmail.com>
2020-02-22 19:59:11 +0100

    headers

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ var app = express();
 app.use(express.static(__dirname));
 
 app.get('/', function (req, res) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     res.sendFile('/index.html', { root : __dirname});
 });
 
